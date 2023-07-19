120af8285c674130c6d0ed30bda44ea198f6a2e0
Christopher Wiggins <cwiggins610@gmail.com>
2023-07-18 20:38:19 -0400

    deleted script.js moved to main.js

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
-// console.log("connected");
+document.querySelector("#search").addEventListener("click", getPokemon);
 
-// const searchTermsInput = document.body.querySelector("pkmn-form");
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
-// const getPkmnCategories = async () => {
-//   const pkmnTypeCategoriesURL = "https://pokeapi.co/api/v2/type/3";
+function lowerCaseName(string) {
+  return string.toLowerCase();
+}
 
-//   const response = await fetch(pkmnTypeCategoriesURL);
-// };
+function getPokemon(e) {
+  const name = document.querySelector("#pokemonName").value;
+  const pokemonName = lowerCaseName(name);
+  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const moves = data.moves.map((move) => move.move.name).join(", ");
+      document.querySelector(".pokemonBox").innerHTML = `
+          <div>
+            <img src="${
+              data.sprites.other["official-artwork"].front_default
+            }" alt="${capitalizeFirstLetter(data.name)}}" />
+          </div>
+          <div class="pokemonInfo">
+            <h1>${capitalizeFirstLetter(data.name)}</h1>
+            <p>Number: ${data.id}</p>
+            <p>Weight: ${data.weight} lbs</p>
+            <p>Moves: ${moves}</p>
+          </div>
+        `;
+    })
+    .catch((err) => {
+      console.log("Pokemon not found", err);
+    });
+}
 
-// const handleFormInputFocus = () => {
-//   console.log("in focus");
-
-//   getPkmnCategories();
-// };
-
-// searchTermsInput.addEventListener("focus", handleFormInputFocus);
+e.preventDefault();
---
+++ notes.md
@@ -0,0 +1,4 @@
+div pokemonBox = pokemon information
+
+pulls array from API and sperates by commas. (for moves,types,egg groups etc)
+const \__ = data._**_.map((_**) => **_._**.name).join(", ");
